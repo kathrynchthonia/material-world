571ff0914033054800ae397ea84cc96b7a45809a
Your NameTony <tony@wheelertechconsulting.com>
2018-11-14 22:51:03 -0600

    Materialize Boilerplate Setup

---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <!--Import Google Icon Font-->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+  <!--Import materialize.css-->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  <!-- Custom styles for this template -->
+  <link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet">
+
+
+
+  <!--Let browser know website is optimized for mobile-->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Catholic Insurance Man</title>
+</head>
+
+<body>
+  <!-- Header -->
+  <div class="navbar-fixed">
+    <nav class="blue darken-1"></nav>
+  </div>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php get_header(); ?>
+
+<h1 class="btn blue white-text">Help!</h1>
+
+<?php get_footer(); ?>
