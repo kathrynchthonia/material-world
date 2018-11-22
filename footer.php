05297774970b21d616b94ca6a808264905158b44
Your NameTony <tony@wheelertechconsulting.com>
2018-11-22 00:29:04 -0600

    Material world v0.9.2 Beta Removed jQuery

--- footer.php
+++ footer.php
@@ -71,11 +71,6 @@
         ScrollReveal().reveal('.sr31');
         ScrollReveal().reveal('.sr32');
     </script>
-    <!-- Jquery -->
-    <script
-      src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-      integrity="sha256-3edrmyuQ0w65f8gfBsqowzjJe2iM6n0nKciPUp8y+7E="
-      crossorigin="anonymous"></script>
     <!-- Materialize JS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     <script>
